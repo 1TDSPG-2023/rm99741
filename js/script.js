49fd6bd670a01888234ac667ebc2bc9cc1e640d8
amandabomfim <rm99741@fiap.com.br>
2023-05-05 11:59:57 -0300

    Concatenação com SPREAD

--- js/script.js
+++ js/script.js
@@ -39,25 +39,25 @@
 //} );
 
 //ARRAYS E SEU M[ETODOS]
-let frutas = ["banana","maçã","pêra","uva","melancia","ameixa","morango","goiaba"];
-console.log(frutas);
-console.log("FRUTA : " + frutas[4])
+//let frutas = ["banana","maçã","pêra","uva","melancia","ameixa","morango","goiaba"];
+//console.log(frutas);
+//console.log("FRUTA : " + frutas[4])
 //Descobrindo o tamanho do array através da propriedade length
-console.log(`Tamanho do Array : ${frutas.length}`);
+//console.log(`Tamanho do Array : ${frutas.length}`);
 
 //Adicionando um iten ao final do array com o método push(itenNome);
-frutas.push("fruta-conde");
-console.table(frutas);
+//frutas.push("fruta-conde");
+//console.table(frutas);
 //Adicionando um iten no inicio do array com o método unshift(itenNome);
-frutas.unshift("jabuticaba");
-console.log(frutas);
+//frutas.unshift("jabuticaba");
+//console.log(frutas);
 
 //Remover um iten do final do array com o método pop();
-frutas.pop();
-console.log(frutas);
+//frutas.pop();
+//console.log(frutas);
 //Remover um iten do inicio do array com o método shift();
-frutas.shift();
-console.log(frutas);
+//frutas.shift();
+//console.log(frutas);
 
 //Localizando um item com o método indexOf(itenNome);
 //Obs: O indexOf(itenNome), vai retornar o indice onde se encontra o iten buscado.
@@ -68,16 +68,34 @@ console.log(frutas);
 //Removendo um indice uma determinada quantidade de vezes utilizando o método splice.
 //slipece(indice do iten, quantidade de vezes a ser removido);
 //Obs: Para obter o indice utilize o método indexOf(itemNome);
-let indice = frutas.indexOf("ameixa");
-console.log(indice);
-console.log(`FRUTA QUE ESTAVA NO INDICE ANTES DA REMOÇÃO : ${frutas[indice]}`);
+//let indice = frutas.indexOf("ameixa");
+//console.log(indice);
+//console.log(`FRUTA QUE ESTAVA NO INDICE ANTES DA REMOÇÃO : ${frutas[indice]}`);
 //Aplicando o splice e usando o indice obtido com indexOf.
-frutas.splice(indice,1);
-console.log(`FRUTA QUE FICOU NO INDICE DEPOIS DA REMOÇÃO : ${frutas[indice]}`);
+//frutas.splice(indice,1);
+//console.log(`FRUTA QUE FICOU NO INDICE DEPOIS DA REMOÇÃO : ${frutas[indice]}`);
 
 //Percorrer o ARRAY utilizando o forEach
-frutas.forEach( (fruta)=>{
-    console.log(`Olha ela : ${fruta}`);
-} );
+//frutas.forEach( (fruta)=>{
+//    console.log(`Olha ela : ${fruta}`);
+//} );
 
+let nr1 = [1,2,3,4,5];
+let nr2 = [6,7,8,9,10];
+console.log(`ARRAY 1: ${nr1}`);
+console.log(`ARRAY 2: ${nr2}`);
+//Concatenando arrays
+let nr3 = [nr1,nr2];
+console.log(`NOVO ARRAY: ${nr3}`);
+//Percorrendo o novo array com forEach
+nr3.forEach((nr)=>{
+    console.log(`ITEM DO NOVO ARRAY: ${nr}`);
+});
 
+//Concatenando o array com o operador SPREAD( ... )
+nr3 = [...nr1,...nr2];
+console.log(`NOVO ARRAY COM SPREAD: ${nr3}`);
+//Percorrendo o novo array com forEach
+nr3.forEach((nr)=>{
+    console.log(`ITEM DO NOVO ARRAY COM SPREAD: ${nr}`);
+});
